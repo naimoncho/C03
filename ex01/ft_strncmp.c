c088d9e0c9043f26d7a7b8f2d01cba96a332dd8c
Naim Cheniouni Gomez <ncheniou@c2r13s1.42madrid.com>
2024-11-16 19:13:03 +0100

    yeah

---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,28 @@
+// #include <stdio.h>
+// #include <string.h>
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0' && (i < n))
+    {
+        if (s1[i] != s2[i])
+        {
+            return(s1[i] - s2[i]);
+        }
+        i ++;
+    }
+    return(0);
+}
+// int main()
+// {
+//     unsigned int n;
+
+//     n = 7;
+//     char s1[] = "to tus muertos";
+//     char s2[] = "tu tas muertos";
+//     printf("%d\n", strncmp(s1,s2,n));
+//     printf("%d", ft_strncmp(s1,s2,n));
+// }
---
+++ ex02/ft_strcat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+char *ft_strcat(char *dest, char *src)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (dest[i] != '\0')
+    {
+        i ++;
+    }
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i ++;
+        j ++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int main()
+{
+    char src[] = "Adioooos";
+    char dest[50] = "Holaa";
+    printf ("%s\n", strcat(dest, src));
+    printf ("%s", dest);
+    return 0;
+}
